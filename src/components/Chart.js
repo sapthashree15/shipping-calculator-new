b2606627203a79febac1e8a6cc9973650ffd5c98
sapthashree15 <kssapthashree@gmail.com>
2023-08-02 21:02:58 +0530

    changes made from theia lab environment

--- src/components/Chart.js
+++ src/components/Chart.js
@@ -1,14 +1,13 @@
 import React from 'react';
-import '../styles.css'; // Import the styles.css file
 
 const Chart = ({ freightRates }) => {
   return (
-    <div className="chart-container"> {/* Add the class name for container */}
+    <div className="chart-container">
       <h2>Shipping Rate Chart</h2>
       <table>
         <thead>
           <tr>
-            <th>Package Weight</th>
+            <th>Package Weight </th>
             <th>Destination</th>
             <th>Shipping Rate</th>
           </tr>
@@ -18,7 +17,6 @@ const Chart = ({ freightRates }) => {
             <tr key={index}>
               <td>{rate.packageSize}</td>
               <td>{rate.destination}</td>
-              {/* Display rates with the appropriate currency symbol */}
               <td>{`${rate.currency === 'usd' ? '$' : '₹'}${rate.rate.toFixed(2)}`}</td>
             </tr>
           ))}
--- src/components/ShippingCalculator.js
+++ src/components/ShippingCalculator.js
@@ -2,14 +2,16 @@ import React, { useState } from 'react';
 import Chart from './Chart';
 import LoginForm from './LoginForm';
 import '../styles.css'; // Import the styles.css file
+import Select from 'react-select'; // Import the react-select component
+import cityOptions from '../components/destinationList'; // Import the city and state options
 
 const ShippingCalculator = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [freightRates, setFreightRates] = useState([]);
   const [packageSize, setPackageSize] = useState('');
-  const [destination, setDestination] = useState('');
+  const [destination, setDestination] = useState(null);
   const [rate, setRate] = useState('');
-  const [packageWeightUnit, setPackageWeightUnit] = useState('kg'); // Add package weight unit state
+  const [packageWeightUnit, setPackageWeightUnit] = useState('kg');
 
   const handleLogin = () => {
     setIsLoggedIn(true);
@@ -21,14 +23,15 @@ const ShippingCalculator = () => {
       return;
     }
 
-    // Determine the currency based on the package weight unit
     const currency = packageWeightUnit === 'kg' ? 'inr' : 'usd';
 
-    // Create a new rate object and add it to the existing rates array
-    const newRate = { packageSize, destination, rate: parseFloat(rate), currency };
+    const newRate = { packageSize, destination: destination.label, rate: parseFloat(rate), currency };
     setFreightRates([...freightRates, newRate]);
+
     setPackageSize('');
-    setDestination('');
+    // Retain the previously selected city name
+    // while clearing the packageSize and rate fields
+    setDestination(destination);
     setRate('');
   };
 
@@ -40,26 +43,32 @@ const ShippingCalculator = () => {
         <div className="shipping-calculator-container">
           <h1>Admin Dashboard</h1>
           <div>
-            <h2>Add Freight Rate</h2>
-            <div className="package-input">
+          <h2>Add Freight Rate</h2>
+          <form>
+            <div className="package-weight-container">
               <input
                 type="text"
                 placeholder="Package Weight"
                 value={packageSize}
                 onChange={(e) => setPackageSize(e.target.value)}
+                className="package-size-input" // Add the class to the input
               />
-              {/* Add the dropdown for package weight unit selection */}
-              <select value={packageWeightUnit} onChange={(e) => setPackageWeightUnit(e.target.value)}>
+              <select
+                value={packageWeightUnit}
+                onChange={(e) => setPackageWeightUnit(e.target.value)}
+              >
                 <option value="kg">kg</option>
                 <option value="lb">lb</option>
               </select>
             </div>
-            <input
-              type="text"
-              placeholder="Destination"
-              value={destination}
-              onChange={(e) => setDestination(e.target.value)}
-            />
+            <div className="city-state-dropdown">
+              <Select
+                options={cityOptions}
+                value={destination}
+                onChange={(selectedOption) => setDestination(selectedOption)}
+                placeholder="Select City and State"
+              />
+            </div>
             <div className="shipping-rate-input">
               <input
                 type="text"
@@ -67,16 +76,15 @@ const ShippingCalculator = () => {
                 value={rate}
                 onChange={(e) => setRate(e.target.value)}
               />
-              {/* Add the dropdown for currency selection */}
               <select value={packageWeightUnit === 'kg' ? 'inr' : 'usd'} onChange={(e) => setPackageWeightUnit(e.target.value === 'inr' ? 'kg' : 'lb')}>
                 <option value="inr">INR</option>
                 <option value="usd">USD</option>
               </select>
             </div>
             <button onClick={addFreightRate}>Add Rate</button>
+            </form>
           </div>
-          {/* Pass the freightRates to the Chart component */}
-          <Chart freightRates={freightRates} />
+          <Chart freightRates={freightRates} packageWeightUnit={packageWeightUnit} />
         </div>
       )}
     </div>
